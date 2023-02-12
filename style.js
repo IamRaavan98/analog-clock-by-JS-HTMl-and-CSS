cb08b06e9c92c63e3ad43b198a9e48fd0d2df7a4
Rohan Agrawal <Rohanagrawal1798@gmail.com>
2023-02-12 17:53:41 +0530

    first commit

---
+++ style.js
@@ -0,0 +1,23 @@
+setInterval(setClock, 1000)
+setClock()
+function setClock(){
+    const hourHand = document.querySelector(".hour")
+    const minuteHand = document.querySelector(".minute")
+    const secondHand = document.querySelector(".second")
+    const currentDate = new Date()
+    const secondRatio = currentDate.getSeconds() /60 ;
+    const minutesRatio = (secondRatio + currentDate.getMinutes()) /60;
+    const hourRatio = (minutesRatio+currentDate.getHours()) /12;
+   console.log();
+    setRotation(secondHand,secondRatio)
+    setRotation(minuteHand,minutesRatio)
+    setRotation(hourHand,hourRatio)
+    
+}
+
+function setRotation(element, rotationRatio){
+    console.log(element,rotationRatio,rotationRatio*360 );
+element.style.setProperty('--rotation',rotationRatio * 360)
+}
+
+
